f488c17d467f66bc5c2a7f631e75f4e23aef2c1d
Talal-Alkhamis-C77 <70804941+Talal-Alkhamis-C77@users.noreply.github.com>
2022-03-16 19:51:10 +0300

    hi

--- d5-cw1.playground/Contents.swift
+++ d5-cw1.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let family = ["Talal", "Adnan", "Jacob", "Mewo"]
+print(family)
+
+var evenNumbers = [2,4,6,8,10,12]
+    evenNumbers.remove(at: 2)
+evenNumbers += [16,18,10,22]
+
+var money = [1,2,50]
+money.randomElement()
+
+var wow = [1,2,3,4,5]
+for i in 1...5 {
+    print(i)
+}
+
